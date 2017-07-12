8de9fe0decf4be07104315b0974f7b90718fdab4
jdyen <jdl.yen@gmail.com>
2017-07-12 17:39:27 +1000

    update zzz file

--- R/zzz.R
+++ R/zzz.R
@@ -1,14 +1,5 @@
 .onLoad <- function(libname, pkgname) {
-  modules <- paste0('growmod', names(stanmodels), '_mod')
+  modules <- paste0('stan_fit4', names(stanmodels), '_mod')
   for (m in modules) loadModule(m, what = TRUE)
-}
-
-.onAttach <- function(...) {
-  growmodLib <- dirname(system.file(package = 'growmod'))
-  pkgdesc <- suppressWarnings(utils::packageDescription('growmod', lib.loc = growmodLib))
-  if (length(pkgdesc) > 1) {
-    builddate <- gsub(';.*$', '', pkgdesc$Packaged)
-    packageStartupMessage(paste('growmod (Version ', pkgdesc$Version, ', packaged: ', builddate, ')', sep ''))
-  }
 }
 
