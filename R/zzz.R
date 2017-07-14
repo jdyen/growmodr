c1307e2962fa9d9e0ca49b7a2c45cd0208eb3954
jdyen <jdl.yen@gmail.com>
2017-07-14 17:17:31 +1000

    update onload steps

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .onLoad <- function(libname, pkgname) {
-  modules <- paste0('stan_fit4', names(stanmodels), '_mod')
+  modules <- paste0('stan_fit4', names(growmod:::stanmodels), '_mod')
   for (m in modules) loadModule(m, what = TRUE)
 }
 
