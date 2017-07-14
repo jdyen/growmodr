5620cbbe3361a150d29eb903e6a8ba061e666453
jdyen <jdl.yen@gmail.com>
2017-07-14 18:25:29 +1000

    update onload call

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,5 @@
 .onLoad <- function(libname, pkgname) {
   modules <- paste0('stan_fit4', names(growmod:::stanmodels), '_mod')
-  print(modules)
   for (m in modules) loadModule(m, what = TRUE)
 }
 
