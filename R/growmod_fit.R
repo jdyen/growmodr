fa9ce3a045f6ee96192210bf1c16f1225223e9a7
jdyen <jdl.yen@gmail.com>
2017-07-19 14:40:18 +1000

    update multi model without preds

--- R/growmod_fit.R
+++ R/growmod_fit.R
@@ -424,10 +424,7 @@ growmod.default <- function(x,
   } else {
     data_set <- vector('list', length = length(model))
     for (i in seq(along = model)) {
-      print(class(pred_set))
-      print(length(pred_set))
-      print(class(pred_set[[i]]))
-      if (!is.null(pred_set[[i]])) {
+      if (length(pred_set) == length(model)) {
         data_set[[i]] <- growmod_data(data_set = list(size = size,
                                                       index = index,
                                                       block = block,
